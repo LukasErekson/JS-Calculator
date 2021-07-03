3265fce55f7491152edc9c5c40e2d124bd055956
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-07-03 14:05:52 -0600

    Add starting operating capabilities
    - Link the stylesheet and script to the HTML file properly
    - Allow the user to hit '=' to get answers to operations.

--- scripts/calc-functions.js
+++ scripts/calc-functions.js
@@ -16,6 +16,14 @@ function divide(num1, num2) {
   return +num1 / +num2;
 }
 
+/**
+ * Takes in the operating parameters and actualyl runs the desired
+ * binary operation.
+ * @param {String} operator The operator to use with the two numbers.
+ * @param {String} num1str The first (left) number in the operation.
+ * @param {String} num2str The second (right) number in the operation.
+ * @returns 
+ */
 function operate(operator, num1str, num2str) {
   switch (operator) {
     case '+':
@@ -34,33 +42,86 @@ function operate(operator, num1str, num2str) {
 
 let displayStr = document.getElementById("current-op-str");
 
+/**
+ * Reset the display so that it's empty.
+ */
 function clearDisplay() {
   displayStr.innerText = '';
 }
+
+/**
+ * Update the display using the approrpiate button target.
+ * @param {Event} event 
+ */
 
 function updateDisplay(event) {
   let value = event.target.innerText;
   if (isNaN(parseFloat(value))) {
     switch (value) {
       case ".":
-        displayStr.innerText += value;
+        if (!decimalInUse) {
+          displayStr.innerText += value;
+          decimalInUse = true;
+        }
         break;
       case "C":
         clearDisplay();
+        lastWasOperator = false;
         break;
       case "=":
+        console.log(lastWasOperator);
+        if (!lastWasOperator) {
+          // Run Calculations
+          let valueArray = displayStr.innerText.split(' ');
+          let newValue = operate(valueArray[1], valueArray[0], valueArray[2]);
+          let strValue = (newValue < 0 ? '-' : '') + String(Math.abs(newValue));
+          console.log(newValue);
+          console.log(strValue);
+          // TODO : Still a bit buggy.
+          // Remove the numbers and operators associated with the last operation.
+          displayStr.innerText = displayStr.innerText.replace(valueArray[1], '');
+          displayStr.innerText = displayStr.innerText.replace(valueArray[0], strValue);
+          displayStr.innerText = displayStr.innerText.replace(valueArray[2], '');
+        }
         break;
       default:
-        displayStr.innerText += ` ${value} `;
+        if (!lastWasOperator) {
+          displayStr.innerText += " " + value;
+          // Reset the decimal for the other number.
+          decimalInUse = false;
+          lastWasOperator = true;
+        }
         break;
     }
     
   }
   else {
-    displayStr.innerText += value;
+    if (lastWasOperator) {
+      displayStr.innerText += " " + value;
+    }
+    else {
+      displayStr.innerText += value;
+    }
+    lastWasOperator = false;
+  }
+}
+
+/**
+ * Finds the first operator in the string to assign as the operator.
+ */
+function findOperator() {
+  let displayText = displayStr.innerText;
+  for (let i = 0; i <= displayText.length; i++) {
+    if (displayText[i] !== '.' && isNaN(parseFloat(displayText[i]))) {
+      return displayText[i];
+    }
   }
 }
 
 let buttons = Array.from(document.querySelectorAll('.numpad-num'));
 
 buttons.forEach((item) => item.addEventListener('click', updateDisplay));
+
+let decimalInUse = false;
+let lastWasOperator = false;
+
